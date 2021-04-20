dbcb4176308037b17a9878987f27c01e8d318b89
Ignacio Cortes <nachocortes250@gmail.com>
2021-04-20 14:00:26 -0300

    Subo programaSimple.c

---
+++ 00-PrimerProgramaC -/programaSimple.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+    printf("Hola mundo! ");
+    printf("Ingrese un numero: ");
+    scanf("%d", &numero);
+    printf("El doble del nuermo que ingreso es = %d", numero *2);
+
+    return 0;
+
+}
