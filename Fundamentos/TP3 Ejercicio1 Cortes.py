5d614100f526e48c71a6ac8747d9e0f9afc6640e
Ignacio Cortes <62978981+nachodcortes@users.noreply.github.com>
2024-03-31 23:16:32 -0300

    Add files via upload

---
+++ Fundamentos/TP3 Ejercicio1 Cortes.py
@@ -0,0 +1,7 @@
+print("Ingrese 2 numeros enteros: ")
+numero1 = int(input())
+numero2 = int(input())
+if numero1 == numero2:
+    print("Son 2 numeros iguales")
+else:
+    print("Son 2 numeros distintos")
